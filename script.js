c84169f25df66a60f76ca3d17fe8dd4c764b5b34
Loxhify <91940766+Loxhify@users.noreply.github.com>
2022-01-10 20:01:19 +0100

    update all

---
+++ script.js
@@ -0,0 +1,18 @@
+var myAge = 25;
+var myfavoriteNumber7 = true;
+var myCar = "BMW";
+
+var firstName = "Temirlan"
+var lastName = "Shangaliyev"
+var fullName = firstName + " " + lastName;
+console.log (fullName);
+
+
+for (var i=7; i<=13; i+=1) {
+    console.log(i)
+}
+
+if (condition)
+
+var orderHasShipped = true;
+var orderHasShipped = false;
